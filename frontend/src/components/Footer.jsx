ae8bde95627facbb72af28f07506c03683d80aed
NavozZ <navodyatheshan4@gmail.com>
2026-06-16 11:49:21 +0530

    built contact section and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,13 +4,19 @@ return(
 
 <footer
 className="
-py-16
+py-20
 text-center
 text-gray-500
 "
 >
 
+<p>
+Built with React • Tailwind • Framer Motion
+</p>
+
+<p className="mt-2">
 © 2026 Navodya Theshan
+</p>
 
 </footer>
 
