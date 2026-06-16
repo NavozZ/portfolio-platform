a5c3cb1710299e3748707c4ae0c8e3687930171a
NavozZ <navodyatheshan4@gmail.com>
2026-06-16 11:54:39 +0530

    setup express backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+
+res.json({
+message:"Portfolio API Running"
+})
+
+})
+
+const PORT=5000
+
+app.listen(PORT,()=>{
+
+console.log(`Server running ${PORT}`)
+
+})
