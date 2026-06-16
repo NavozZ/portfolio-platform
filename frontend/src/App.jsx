1d0d09b32963bab1bf4769a4a1d0e6fba79d7122
NavozZ <navodyatheshan4@gmail.com>
2026-06-17 00:18:02 +0530

    built admin dashboard create project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./components/Projects"
 import Experience from "./components/Experience"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import Admin from "./components/Admin"
 
 export default function App() {
 
@@ -40,6 +41,8 @@ text-white
       <Experience />
 
       <Contact />
+
+      <Admin />
 
       <Footer />
 
